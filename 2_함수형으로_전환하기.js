dfd179f52221ffe3c65057fbfd505182e2a7a09f
rojojun <zoloman316@gmail.com>
2024-12-28 17:23:06 +0900

    2강 다형성 관련 예제 커밋

--- 2_함수형으로_전환하기.js
+++ 2_함수형으로_전환하기.js
@@ -70,3 +70,42 @@ console.log(
 
 // 3. each 만들기
 // 3-1. _each로 _map, _filter 중복 제거
+
+console.log("EACH START >>>> ")
+
+console.log(
+    [1,2,3].map(function (val) {
+        return val * 2;
+    })
+)
+
+// 객체형 프로그래밍, 객체가 중요함 -> 데이터가 중요함 데이터를 평가해야함
+console.log(
+    [1,2,3,4].filter(function (val) {
+        return val % 2;
+    })
+)
+
+// 함수형 프로그래밍, 함수 -> 동작이 중요함 동작으로 데이터를 이야기 하기에 유연성 높아짐
+console.log(
+    _map([1,2,3,4], function(val) {
+        return val * 2;
+    })
+)
+
+// 함수와 메서드는 다르다 -> 메서드는 객체 상태에 따라 값이 달라진다
+// 메서드는 객체 지향 프로그램으로서 클래스의 인스턴스에 종속되어 사용되어진다.
+// 함수는 객체에 종속되어 있지 않아있기 때문에 어떠한 값이 들어오든지 동작에 대한 정의를 할 수 있다.
+
+// 아래 코드는 사용 불가 arrayLike의 문제
+// 객체에 없는 메서드이기 때문에 불가능함 -> 그렇다면 어떠한 방식으로 풀어야할까?
+// 힘수를 정의하여 함수형으로 정의해서 map의 역할을 할 수 있다.
+document.querySelectorAll('*').map(function (node) {
+    return node.nodeName;
+})
+
+// mapper와 같은 두번째 인자는 언제나 콜백함수라고 불려질 수 없다 -> 보조함수를 다들 콜백함수로 부르는 경향이 있음
+// 역할에 맞는 보조함수의 이름이 존재한다.
+_map([1,2,3,4], function(val) {
+    return val * 2;
+})
