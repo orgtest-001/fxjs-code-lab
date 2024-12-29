d96275702eaac0580493d131d4af044d28760eb5
rojojun <zoloman316@gmail.com>
2024-12-29 23:31:06 +0900

    2강 커링 예제

--- 2_함수형으로_전환하기.js
+++ 2_함수형으로_전환하기.js
@@ -100,12 +100,93 @@ console.log(
 // 아래 코드는 사용 불가 arrayLike의 문제
 // 객체에 없는 메서드이기 때문에 불가능함 -> 그렇다면 어떠한 방식으로 풀어야할까?
 // 힘수를 정의하여 함수형으로 정의해서 map의 역할을 할 수 있다.
-document.querySelectorAll('*').map(function (node) {
-    return node.nodeName;
-})
+// document.querySelectorAll('*').map(function (node) {
+//     return node.nodeName;
+// })
 
 // mapper와 같은 두번째 인자는 언제나 콜백함수라고 불려질 수 없다 -> 보조함수를 다들 콜백함수로 부르는 경향이 있음
 // 역할에 맞는 보조함수의 이름이 존재한다.
 _map([1,2,3,4], function(val) {
     return val * 2;
 })
+
+// 커링 -> 함수와 인자를 다루는 기법
+// 자바스크립트는 커링이 지원되지 않지만, 1급함수가 지원되고 평가시점을 마음대로 다룰 수 있게 커링과 같은 기법을 구현할 수 있음
+function _curry(fn) {
+    return function (a,  b) {
+        return arguments.length == 2 ? fn(a, b) : function (b) {
+            return fn(a, b);
+        };
+    }
+}
+
+function _curryr(fn) {
+    return function (a, b) {
+        return arguments.length == 2 ? fn(a, b) : function (b) { return fn(b, a); };
+    }
+}
+
+console.log("커링 예시 >>>> ")
+var add = _curry(function (a, b) {
+    return a + b;
+});
+
+var add10 = add(10);
+
+console.log( add10(5) );
+console.log( add(5)(3) );
+console.log( add(1, 2) );
+
+// 원하는 시점까지 들고있다가 평가시점에 값을 출력하는 것이 커링기법
+// 인자로 본체함수 -> 커리함수 실행 -> add(10)은 a에 10의 값을 기억하는 클로져가 된다 -> add10(5)는 기억한 a의 클로져함수를 받아서 5를 더해 15가 된다.
+
+var sub = _curryr(function (a, b) {
+    return a - b;
+});
+
+console.log( sub(10, 5) );
+
+var sub10 = sub(10);
+console.log( sub10(5) );
+
+// 2. _get 만들어 좀 더 간단하게 하기
+var _get = _curryr(function (obj, key) {
+    return obj == null ? undefined : obj[key];
+});
+
+console.log("_get에 대한 예제 >>>> ")
+var user1 = users[0]
+console.log(user1.name);
+console.log(_get(user1, 'name'));
+
+console.log(_get(users[10], 'name'));
+
+console.log(
+    _map(
+        _filter(users, function(user) { return user.age >= 30; }),
+        function (user) { return user.name; }
+    )
+);
+
+console.log(
+    _map(
+        _filter(users, function(user) { return user.age < 30; }),
+        function (user) { return user.age; }
+    )
+);
+
+console.log("TO CURRYING >>>> ")
+
+console.log(
+    _map(
+        _filter(users, function(user) { return user.age >= 30; }),
+        _get('name')
+    )
+);
+
+console.log(
+    _map(
+        _filter(users, function(user) { return user.age < 30; }),
+        _get('age')
+    )
+);
